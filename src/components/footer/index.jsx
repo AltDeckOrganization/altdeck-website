e2ff316fbe1f7d5fc0c212d03caa31d5de6bb627
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-04-27 16:33:48 +0500

    feat: footer added

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,7 +1,34 @@
+import Link from "next/link";
 import React from "react";
+import footerStyles from "./footer.module.css";
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+function Footer() {
+  return (
+    <div className={`${footerStyles.footer} flex items-center`}>
+      <div className="">
+        &copy; 2022{" "}
+        <span className={`font-bold text-lg  ${footerStyles.span}`}>
+          AltDeck
+        </span>
+      </div>
+      <></>
+      <div>
+        <ul
+          className={`flex space-x-8 text-[#808080] text-sm font-bold ${footerStyles.footerUl}`}
+        >
+          <li>
+            <Link href="/tos">TOS</Link>
+          </li>
+          <li>
+            <Link href="/faqs">FAQ</Link>
+          </li>
+          <li>
+            <Link href="/privacy">Privacy</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 export default Footer;
